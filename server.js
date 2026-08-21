af06f6a6209fdaf0e14375d428382d544f0ca0e5
sannihady-create <sannihady@gmail.com>
2026-08-21 12:00:58 +0100

    Update server.js

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.post('/api/register', async (req, res) => {
     );
     res.json({ message: 'Compte créé avec succès !', user: newUser.rows[0] });
   } catch (err) {
-    res.status(400).json({ error: "Email ou nom d'utilisateur déjà utilisé." });
+   res.status(400).json({ error: "Email ou nom d'utilisateur déjà utilisé." });
   }
 });
 
